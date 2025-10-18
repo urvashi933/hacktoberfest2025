3838dfb4805f3e3ee3015adaa6275a889724192e
Urvashi933 <srivastavaurvashi933@gmail.com>
2025-10-18 11:56:22 +0530

    Update BinarySearch.java
    The previous calculation of the middle index as int mid = (low + high) / 2; could suffer from integer overflow if low and high are very large. A safer way to compute the midpoint is:
    int mid = low + (high - low) / 2;
    This avoids overflow by subtracting before adding.

--- DSA/C++/Searching/DSA/Java/Searching/BinarySearch.java
+++ DSA/C++/Searching/DSA/Java/Searching/BinarySearch.java
@@ -4,7 +4,7 @@ public class BinarySearch {
     public static int binarySearch(int[] arr, int x) {
         int low = 0, high = arr.length - 1;
         while (low <= high) {
-            int mid = (low + high) / 2;
+            int mid = low + (high - low) / 2;
             if (arr[mid] == x) return mid;
             else if (arr[mid] < x) low = mid + 1;
             else high = mid - 1;
